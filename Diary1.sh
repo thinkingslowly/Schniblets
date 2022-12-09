d9ecca3637a74110a22faf712fd26cc194d8f0c4
thinkingslowly <113632814+thinkingslowly@users.noreply.github.com>
2022-12-09 17:51:48 +0000

    Update Diary1.sh

--- Diary1.sh
+++ Diary1.sh
@@ -1,15 +1,14 @@
-## this unexpectadly errors , however despite that does what is intended of it.
-## which  is, when run, adds an time record to an existing array ; takes a user input adding to another array and rewrites the Script-file. 
+## This adds an time record to an existing array ; takes a user input converts it to hex adding the converted input to another array and rewrites the Script-file. 
 
 datearray=(hmm 2022-12-09T00:00:37+00:00 2022-12-09T00:01:13+00:00 2022-12-09T00:11:36+00:00 ) ;
 feelingsarray=("my left foot is cold" "I am surviving" "still got a cold left foot"  ) ;
 echo "hello how are you" ;
 startblock () 
 { 
-    echo '#' 'this unexpectadly errors , however despite that does what is intended of it, which  is, when run, add a var to an array and rewrites ';
+    echo '##' ' This adds an time record to an existing array ; takes a user input converts it to hex adding the converted input to another array and rewrites the Script-file.';
     datearray+=($( date -I'seconds' ));
     echo 'datearray=''('"${datearray[@]}" ') ;';
-    feelingsarray+=("$( read  "feelings" ; echo "${feelings[@]}" )");
+    feelingsarray+=("$( read  "feelings" ; echo "${feelings[@]}" | xxd -p )");
     echo -n 'feelingsarray=''(';
     for i in "${feelingsarray[@]}";
     do
