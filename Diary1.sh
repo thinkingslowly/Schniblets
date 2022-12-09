cc4c0f61412d2f4e4cd1cb5d2e7723f836a5893e
thinkingslowly <113632814+thinkingslowly@users.noreply.github.com>
2022-12-09 00:25:10 +0000

    Diary2.sh
    A Script-file that writes the information it receives to itself. 
    Recording a Time and a user response to the question "how are you".

--- Diary1.sh
+++ Diary1.sh
@@ -1,11 +1,23 @@
-# this unexpectadly errors , however despite that does what is intended of it, which  is, when run, add a var to an array and rewrites 
-datearray=(hmm 2022-09-15T19:40:43+01:00 ) ;
+## this unexpectadly errors , however despite that does what is intended of it.
+## which  is, when run, adds an time record to an existing array ; takes a user input adding to another array and rewrites the Script-file. 
+
+datearray=(hmm 2022-12-09T00:00:37+00:00 2022-12-09T00:01:13+00:00 2022-12-09T00:11:36+00:00 ) ;
+feelingsarray=("my left foot is cold" "I am surviving" "still got a cold left foot"  ) ;
+echo "hello how are you" ;
 startblock () 
 { 
     echo '#' 'this unexpectadly errors , however despite that does what is intended of it, which  is, when run, add a var to an array and rewrites ';
     datearray+=($( date -I'seconds' ));
     echo 'datearray=''('"${datearray[@]}" ') ;';
+    feelingsarray+=("$( read  "feelings" ; echo "${feelings[@]}" )");
+    echo -n 'feelingsarray=''(';
+    for i in "${feelingsarray[@]}";
+    do
+        printf \""$i"\"' ';
+    done;
+    echo ' ) ;';
+    printf "echo \"hello how are you\" ;\n";
     declare -f startblock;
-    echo 'startblock > Diary1.sh'
+    echo 'startblock > Diary2.sh'
 }
-startblock > Diary1.sh
+startblock > Diary2.sh
