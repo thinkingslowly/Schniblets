533b2137a3266c07a0da19e570281008be4e31c6
thinkingslowly <113632814+thinkingslowly@users.noreply.github.com>
2023-01-29 22:35:31 +0000

    Update ArraysofArrays.sh

--- ArraysofArrays.sh
+++ ArraysofArrays.sh
@@ -6,3 +6,15 @@ wowzas=(1 12344)
 echo ${wow[@]:${wowzas[1]}:12345}
 
 ## Coincidentally it sometimes can take along time to find out how to do a simple task. 
+
+
+
+somenumbers=( 1 2 3 4 5) ;
+
+wow[1]=1 ;
+
+wowzas=($(echo "${#somenumbers[@]}")) ;
+
+echo ${somenumbers[@]:${wow[1]}:${wowzas[0]}}
+## this strips the array of the first element 
+##output 2 3 4 5
