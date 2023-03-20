7aad5922721a0705d18b9e16857adc3245825fac
thinkingslowly <113632814+thinkingslowly@users.noreply.github.com>
2023-03-20 01:54:18 +0000

    Update soxbendingespeakedwords.sh
    ## To my ears this makes espeak more pleasant sounding.

--- soxbendingespeakedwords.sh
+++ soxbendingespeakedwords.sh
@@ -1,5 +1,5 @@
 ## This apllies a bend to the esepaked output of each individual word in an one element array and plays them as a combined modified unit to the standard audio device. 
-
+## To my ears it makes espeak more pleasnt sounding. 
 bendsize=(-100) ;
 splitwords=$(echo "a block of text to test this thing" | sed "s| |\n|g" ) ;
 ##this input array is not protected from special characters. 
@@ -20,15 +20,17 @@ bendduration=$(echo $x/$y | bc -l) ;
 z=($( bendflow=(0) ; while [[ "$y" -gt 0 ]] ; do bendsize=$(echo 0 - "$bendsize" | bc) ; echo -n "$bendflow,"; echo -n "$bendsize," ; echo "$bendduration" ; y=("$y"-1) ; done  )) ;
 ##builds bend pattern  
 
-precommand+=$( echo -n "'|" ; echo " echo "$i" | espeak  --stdout | sox -t wav -  -t wav - pad 0 "$k" | sox -t wav -  -t wav - gain -9 bend ${z[@]} " ; echo -n "' " ) ; done ; 
+precommand+=$( echo -n "'|" ; echo " echo "$i" | espeak  --stdout | sox -t wav -  -t wav - pad 0 "$k" | sox -t wav -  -t wav - gain -9 bend ${z[@]} tempo 1.15 bass 1.42 treble 1.82 pitch -42" ; echo -n "' " ) ; done ; 
+
+##something here garbles a wav header maybe the chaining of the pad command 
 ## builds a collection of program sub-units to be later fed to a sox command 
-##something here garbles a wav header maybe the lazy chaining of the pad command 
-
-
-command=$( echo -n "sox  " ; echo "$precommand " ; echo -n "  -t wav - | sox -t wav -  -t wav - silence 1 0.1 1% -2 0.1 1% | play - " )
+## this involves trimming silence over a threshold from each individiual espeaked  word modifying gain bass trebble levels and applying the calculated bendpattern. 
+command=$( echo -n "sox  " ; echo "$precommand " ; echo -n "  -t wav - | sox -t wav -  someoutputfile.wav silence 1 0.064 1% -2.4 0.064 1%  " )
 ## combines sub-unit programs applies a silience filter the pipes to play reading from stdinn  
-## this bit needs converting from a play to a sox command if a commands output is to be stored or further modified. 
 
 eval $command
 
 ##too do change array names and make the silence more responsive to rhythmic context therby reducing the choppiness of the audio flow. 
+## sanitise input from unwanted characters 
+## add a gui for entering a block of text
+## add an option to choose between playing OR recording input. 
